21eaae39b23e485ce3381f373b8a13e9472c01cb
HuangTingWei <0978wally@gmail.com>
2025-01-10 12:14:16 +0800

    .

---
+++ arduino/main/main.ino
@@ -0,0 +1,35 @@
+int RPin = 3, GPin = 4, BPin = 5;
+
+void setup() {
+   Serial.begin(9600); 
+   pinMode(LED_BUILTIN, OUTPUT);
+   pinMode(RPin, OUTPUT);
+   pinMode(GPin, OUTPUT);
+   pinMode(BPin, OUTPUT);
+   
+}
+
+void loop() {
+   
+    if (Serial.available() > 0) {
+        String inByte = Serial.readStringUntil('*');
+        
+        if(inByte == "1") digitalWrite(LED_BUILTIN, 1);
+        else if(inByte == "0") digitalWrite(LED_BUILTIN, 0);
+        else if(inByte == "redLight"){
+            digitalWrite(RPin, 1);
+            digitalWrite(GPin, 0);
+            digitalWrite(BPin, 0);
+        }
+        else if(inByte == "yellowLight"){
+            digitalWrite(RPin, 1);
+            digitalWrite(GPin, 1);
+            digitalWrite(BPin, 0);
+        }
+        else if(inByte == "greenLight"){
+            digitalWrite(RPin, 0);
+            digitalWrite(GPin, 1);
+            digitalWrite(BPin, 0);
+        }
+    } 
+}
